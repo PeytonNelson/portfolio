dd2beddb796066629928a1f364d141a3f2b3dd2f
PeytonNelson <53664023+PeytonNelson@users.noreply.github.com>
2021-11-03 00:00:44 -0500

    scroll reveal contact list

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
-  var strPhrases = {
-    strings: ["Software Engineer", "Developer", "Problem Solver"],
-    typeSpeed: 80,
-    backSpeed: 65,
-    loop: true,
-  };
-  
-  var typed = new Typed('#typedPhrases', strPhrases);
+var strPhrases = {
+  strings: ["Software Engineer", "Developer", "Problem Solver"],
+  typeSpeed: 80,
+  backSpeed: 65,
+  loop: true,
+};
+
+var typed = new Typed('#typedPhrases', strPhrases);
+
+ScrollReveal().reveal('.contact-list', {
+    delay: 500,
+    duration: 100,
+    reset: true
+});
